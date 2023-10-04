324468edf1faa50a21b17208ea9ac190a3154238
claravanstaden <Cats 4 life!>
2023-10-04 11:32:02 +0200

    runtime tests

--- cumulus/parachains/integration-tests/emulated/bridges/bridge-hub-rococo/src/tests/snowbridge.rs
+++ cumulus/parachains/integration-tests/emulated/bridges/bridge-hub-rococo/src/tests/snowbridge.rs
@@ -15,16 +15,15 @@
 
 use crate::*;
 use snowbridge_control;
+use integration_tests_common::BridgeHubRococoPallet;
 
 #[test]
 fn create_agent() {
 	let sudo_origin = <Rococo as Chain>::RuntimeOrigin::root();
 	let destination = Rococo::child_location_of(BridgeHubRococo::para_id()).into();
-	let weight_limit = WeightLimit::Unlimited;
-	let check_origin = None;
 
 	let remote_xcm = VersionedXcm::from(Xcm(vec![
-		UnpaidExecution { weight_limit, check_origin },
+		UnpaidExecution { weight_limit: WeightLimit::Unlimited, check_origin: None },
 		DescendOrigin(X1(Parachain(1000))),
 		Transact {
 			require_weight_at_most: 3000000000.into(),
@@ -63,6 +62,62 @@ fn create_agent() {
 					..
 				}) => {},
 				RuntimeEvent::EthereumControl(snowbridge_control::Event::CreateAgent {
+					..
+				}) => {},
+			]
+		);
+	});
+}
+
+#[test]
+fn create_channel() {
+	let sudo_origin = <Rococo as Chain>::RuntimeOrigin::root();
+	let destination = Rococo::child_location_of(BridgeHubRococo::para_id()).into();
+
+	let remote_xcm = VersionedXcm::from(Xcm(vec![
+		UnpaidExecution { weight_limit: WeightLimit::Unlimited, check_origin: None },
+		DescendOrigin(X1(Parachain(1000))),
+		Transact {
+			require_weight_at_most: 8000000000.into(),
+			origin_kind: OriginKind::Xcm,
+			call: vec![51, 2].into(),
+		}
+	]));
+
+	//BridgeHubRococo::execute_with(|| { // TODO Create agent in storage
+	//	<BridgeHubRococo as BridgeHubRococoPallet>::EthereumControl::create_agent(sudo_origin);
+	//});
+
+	//Rococo Global Consensus
+	// Send XCM message from Relay Chain to Bridge Hub source Parachain
+	Rococo::execute_with(|| {
+		assert_ok!(<Rococo as RococoPallet>::XcmPallet::send(
+			sudo_origin,
+			bx!(destination),
+			bx!(remote_xcm),
+		));
+
+		type RuntimeEvent = <Rococo as Chain>::RuntimeEvent;
+
+		assert_expected_events!(
+			Rococo,
+			vec![
+				RuntimeEvent::XcmPallet(pallet_xcm::Event::Sent { .. }) => {},
+			]
+		);
+	});
+
+	BridgeHubRococo::execute_with(|| {
+		type RuntimeEvent = <BridgeHubRococo as Chain>::RuntimeEvent;
+
+		assert_expected_events!(
+			BridgeHubRococo,
+			vec![
+				RuntimeEvent::DmpQueue(cumulus_pallet_dmp_queue::Event::ExecutedDownward {
+					outcome: Outcome::Complete(_),
+					..
+				}) => {},
+				RuntimeEvent::EthereumControl(snowbridge_control::Event::CreateChannel {
 					..
 				}) => {},
 			]
