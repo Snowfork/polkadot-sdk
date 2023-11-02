0ccbcbfe5cab6333be12f615c3b2ed04ebd8da56
ron <yrong1997@gmail.com>
2023-11-02 17:16:27 +0800

    Improve integration tests

--- cumulus/parachains/integration-tests/emulated/bridges/bridge-hub-rococo/src/tests/snowbridge.rs
+++ cumulus/parachains/integration-tests/emulated/bridges/bridge-hub-rococo/src/tests/snowbridge.rs
@@ -14,8 +14,10 @@
 // limitations under the License.
 
 use crate::*;
+use hex_literal::hex;
 use integration_tests_common::BridgeHubRococoPallet;
 use snowbridge_control;
+use snowbridge_router_primitives::inbound::{Command, MessageV1, VersionedMessage};
 
 #[test]
 fn create_agent() {
@@ -91,7 +93,7 @@ fn create_channel() {
 		Rococo::child_location_of(BridgeHubRococo::para_id()).into();
 
 	let create_agent_xcm = VersionedXcm::from(Xcm(vec![
-		UnpaidExecution { weight_limit: WeightLimit::Unlimited, check_origin: None },
+		UnpaidExecution { weight_limit: Unlimited, check_origin: None },
 		DescendOrigin(X1(Parachain(1000))),
 		Transact {
 			require_weight_at_most: 3000000000.into(),
@@ -148,6 +150,57 @@ fn create_channel() {
 				RuntimeEvent::EthereumControl(snowbridge_control::Event::CreateChannel {
 					..
 				}) => {},
+			]
+		);
+	});
+}
+
+#[test]
+fn register_token() {
+	BridgeHubRococo::fund_accounts(vec![(
+		BridgeHubRococo::sovereign_account_id_of(MultiLocation {
+			parents: 1,
+			interior: X1(Parachain(1000)),
+		}),
+		5_000_000 * ROCOCO_ED,
+	)]);
+
+	// Fund gateway sovereign in asset hub
+	AssetHubRococo::fund_accounts(vec![(
+		hex!("c9794dd8013efb2ad83f668845c62b373c16ad33971745731408058e4d0c6ff5").into(),
+		5_000_000_000_000 * ROCOCO_ED,
+	)]);
+
+	BridgeHubRococo::execute_with(|| {
+		type RuntimeEvent = <BridgeHubRococo as Chain>::RuntimeEvent;
+		type EthereumInboundQueue =
+			<BridgeHubRococo as BridgeHubRococoPallet>::EthereumInboundQueue;
+		let message = VersionedMessage::V1(MessageV1 {
+			chain_id: 15,
+			command: Command::RegisterToken {
+				gateway: hex!("EDa338E4dC46038493b885327842fD3E301CaB39").into(),
+				token: hex!("87d1f7fdfEe7f651FaBc8bFCB6E086C278b77A7d").into(),
+			},
+		});
+		let xcm = EthereumInboundQueue::do_convert(message).unwrap();
+		let _ = EthereumInboundQueue::send_xcm(xcm, 1000.into()).unwrap();
+
+		assert_expected_events!(
+			BridgeHubRococo,
+			vec![
+				RuntimeEvent::XcmpQueue(cumulus_pallet_xcmp_queue::Event::XcmpMessageSent { .. }) => {},
+			]
+		);
+	});
+
+	AssetHubRococo::execute_with(|| {
+		type RuntimeEvent = <AssetHubRococo as Chain>::RuntimeEvent;
+
+		assert_expected_events!(
+			AssetHubRococo,
+			vec![
+				RuntimeEvent::ForeignAssets(pallet_assets::Event::Created { .. }) => {},
+				RuntimeEvent::XcmpQueue(cumulus_pallet_xcmp_queue::Event::Success { .. }) => {},
 			]
 		);
 	});
