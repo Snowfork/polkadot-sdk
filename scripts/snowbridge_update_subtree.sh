1cb02afd0754853a6e82d120546eb32107cae391
claravanstaden <Cats 4 life!>
2023-12-15 14:34:12 +0200

    fixes up subtree

--- scripts/snowbridge_update_subtree.sh
+++ scripts/snowbridge_update_subtree.sh
@@ -7,44 +7,36 @@
 
 set -e
 
-SNOWBRIDGE_BRANCH="${BRANCH:-main}"
+SNOWBRIDGE_BRANCH="${SNOWBRIDGE_BRANCH:-main}"
 POLKADOT_SDK_BRANCH="${POLKADOT_SDK_BRANCH:-master}"
 SNOWBRIDGE_TARGET_DIR="${TARGET_DIR:-bridges/snowbridge}"
 
 function fetch() {
     # the script is able to work only on clean git copy
-    #[[ -z "$(git status --porcelain)" ]] || {
-    #    echo >&2 "The git copy must be clean (stash all your changes):";
-    #    git status --porcelain
-    #    exit 1;
-    #}
-    add_parachain_dir
+    [[ -z "$(git status --porcelain)" ]] || {
+        echo >&2 "The git copy must be clean (stash all your changes):";
+        git status --porcelain
+        exit 1;
+    }
+
     local snowbridge_remote=$(git remote -v | grep "snowbridge.git (fetch)" | head -n1 | awk '{print $1;}')
     if [ -z "$snowbridge_remote" ]; then
-        echo ""
         echo "Adding new remote: 'snowbridge' repo..."
-        echo ""
         git remote add -f snowbridge https://github.com/Snowfork/snowbridge.git
         snowbridge_remote="snowbridge"
     else
-        echo ""
         echo "Fetching remote: '${snowbridge_remote}' repo..."
-        echo ""
         git fetch https://github.com/Snowfork/snowbridge.git --prune
     fi
 
-    echo ""
     echo "Syncing/updating subtree with remote branch '${snowbridge_remote}/$SNOWBRIDGE_BRANCH' to target directory: '$SNOWBRIDGE_TARGET_DIR'"
-    echo ""
     git subtree pull --prefix=$SNOWBRIDGE_TARGET_DIR ${snowbridge_remote} $SNOWBRIDGE_BRANCH --squash
 }
 
 function clean() {
-    echo ""
     echo "Patching/removing unneeded stuff from subtree in target directory: '$SNOWBRIDGE_TARGET_DIR'"
-    remove_parachain_dir
-    chmod +x $SNOWBRIDGE_TARGET_DIR/scripts/verify-pallets-build.sh
-    $SNOWBRIDGE_TARGET_DIR/scripts/verify-pallets-build.sh --ignore-git-state --no-revert
+    chmod +x $SNOWBRIDGE_TARGET_DIR/parachain/scripts/verify-pallets-build.sh
+    $SNOWBRIDGE_TARGET_DIR/parachain/scripts/verify-pallets-build.sh --ignore-git-state --no-revert
 }
 
 function create_patch() {
@@ -53,35 +45,8 @@ function create_patch() {
         git status --porcelain
         exit 1;
     }
-    echo "Creating diff patch file to apply to snowbridg. No Cargo.toml files will be included in the patch."
-    add_parachain_dir
+    echo "Creating diff patch file to apply to snowbridge. No Cargo.toml files will be included in the patch."
     git diff snowbridge/$SNOWBRIDGE_BRANCH $POLKADOT_SDK_BRANCH:bridges/snowbridge --diff-filter=ACM -- . ':(exclude)*/Cargo.toml' > snowbridge.patch
-    remove_parachain_dir
-}
-
-function remove_parachain_dir() {
-    SOURCE_DIR="bridges/snowbridge/parachain"
-    TARGET_DIR="bridges/snowbridge"
-    if [[ -e $SOURCE_DIR ]]; then
-        echo "Removing parachain dir"
-        rm -r $TARGET_DIR/scripts # because there's scripts in both dirs
-        mv $SOURCE_DIR/* $TARGET_DIR/
-
-        rm -r $SOURCE_DIR
-    else
-        echo "Parachain dir already cleared"
-    fi
-}
-
-function add_parachain_dir() {
-    SOURCE_DIR="bridges/snowbridge"
-    TARGET_DIR="bridges/snowbridge/parachain"
-    if [[ -e $SOURCE_DIR ]]; then
-        echo "Parachain dir already added"
-    else
-        mkdir -p $TARGET_DIR
-        mv $SOURCE_DIR/* $TARGET_DIR/
-    fi
 }
 
 case "$1" in
