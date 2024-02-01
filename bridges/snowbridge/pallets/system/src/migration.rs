27677008eadf2eb7f3d6d7cfaad46f7d0a974769
claravanstaden <Cats 4 life!>
2024-02-01 16:09:59 +0200

    adds files

--- bridges/snowbridge/pallets/system/src/migration.rs
+++ bridges/snowbridge/pallets/system/src/migration.rs
@@ -9,66 +9,66 @@ use log;
 use sp_runtime::TryRuntimeError;
 
 pub mod v0 {
-    use frame_support::{pallet_prelude::*, weights::Weight};
+	use frame_support::{pallet_prelude::*, weights::Weight};
 
-    use super::*;
+	use super::*;
 
-    const LOG_TARGET: &str = "ethereum_system::migration";
+	const LOG_TARGET: &str = "ethereum_system::migration";
 
-    pub struct InitializeOnUpgrade<T, BridgeHubParaId, AssetHubParaId>(
-        sp_std::marker::PhantomData<(T, BridgeHubParaId, AssetHubParaId)>,
-    );
-    impl<T, BridgeHubParaId, AssetHubParaId> OnRuntimeUpgrade
-    for InitializeOnUpgrade<T, BridgeHubParaId, AssetHubParaId>
-        where
-            T: Config,
-            BridgeHubParaId: Get<u32>,
-            AssetHubParaId: Get<u32>,
-    {
-        fn on_runtime_upgrade() -> Weight {
-            if !Pallet::<T>::is_initialized() {
-                Pallet::<T>::initialize(
-                    BridgeHubParaId::get().into(),
-                    AssetHubParaId::get().into(),
-                )
-                    .expect("infallible; qed");
-                log::info!(
+	pub struct InitializeOnUpgrade<T, BridgeHubParaId, AssetHubParaId>(
+		sp_std::marker::PhantomData<(T, BridgeHubParaId, AssetHubParaId)>,
+	);
+	impl<T, BridgeHubParaId, AssetHubParaId> OnRuntimeUpgrade
+		for InitializeOnUpgrade<T, BridgeHubParaId, AssetHubParaId>
+	where
+		T: Config,
+		BridgeHubParaId: Get<u32>,
+		AssetHubParaId: Get<u32>,
+	{
+		fn on_runtime_upgrade() -> Weight {
+			if !Pallet::<T>::is_initialized() {
+				Pallet::<T>::initialize(
+					BridgeHubParaId::get().into(),
+					AssetHubParaId::get().into(),
+				)
+				.expect("infallible; qed");
+				log::info!(
 					target: LOG_TARGET,
 					"Ethereum system initialized."
 				);
-                T::DbWeight::get().reads_writes(2, 5)
-            } else {
-                log::info!(
+				T::DbWeight::get().reads_writes(2, 5)
+			} else {
+				log::info!(
 					target: LOG_TARGET,
 					"Ethereum system already initialized. Skipping."
 				);
-                T::DbWeight::get().reads(2)
-            }
-        }
+				T::DbWeight::get().reads(2)
+			}
+		}
 
-        #[cfg(feature = "try-runtime")]
-        fn pre_upgrade() -> Result<Vec<u8>, TryRuntimeError> {
-            if !Pallet::<T>::is_initialized() {
-                log::info!(
+		#[cfg(feature = "try-runtime")]
+		fn pre_upgrade() -> Result<Vec<u8>, TryRuntimeError> {
+			if !Pallet::<T>::is_initialized() {
+				log::info!(
 					target: LOG_TARGET,
 					"Agents and channels not initialized. Initialization will run."
 				);
-            } else {
-                log::info!(
+			} else {
+				log::info!(
 					target: LOG_TARGET,
 					"Agents and channels are initialized. Initialization will not run."
 				);
-            }
-            Ok(vec![])
-        }
+			}
+			Ok(vec![])
+		}
 
-        #[cfg(feature = "try-runtime")]
-        fn post_upgrade(_: Vec<u8>) -> Result<(), TryRuntimeError> {
-            frame_support::ensure!(
+		#[cfg(feature = "try-runtime")]
+		fn post_upgrade(_: Vec<u8>) -> Result<(), TryRuntimeError> {
+			frame_support::ensure!(
 				Pallet::<T>::is_initialized(),
 				"Agents and channels were not initialized."
 			);
-            Ok(())
-        }
-    }
+			Ok(())
+		}
+	}
 }
