1e63aaeafe614cd5f5e83845933fed548af8b32b
claravanstaden <Cats 4 life!>
2024-02-25 15:19:43 +0200

    revert scripts

--- bridges/snowbridge/scripts/run-tests-all-features.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-for package in $(cargo metadata --format-version=1 --no-deps | jq -r '.packages[] | select(.name | startswith("snowbridge")) | .name'); do
-    echo "Running tests for package: $package"
-    cargo test -p "$package" --all-features
-done
--- bridges/snowbridge/scripts/run-tests-benchmarks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-for package in $(cargo metadata --format-version=1 --no-deps | jq -r '.packages[] | select(.name | startswith("snowbridge")) | .name'); do
-    echo "Running benchmarks tests for package: $package"
-    cargo test -p "$package" --features runtime-benchmarks
-done
