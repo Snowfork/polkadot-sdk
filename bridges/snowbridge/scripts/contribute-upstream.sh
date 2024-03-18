cc2405088be02bc97dbe0e501a6cb42643664ed9
claravanstaden <Cats 4 life!>
2024-03-18 08:08:10 +0200

    remove merge from upstream

--- bridges/snowbridge/scripts/contribute-upstream.sh
+++ bridges/snowbridge/scripts/contribute-upstream.sh
@@ -77,9 +77,3 @@ git remote -v | grep -w parity || git remote add parity https://github.com/parit
 git fetch parity master
 git checkout parity/master -- .github
 git add -- .github
-
-# Fetch the latest from parity master
-echo "Fetching latest from Parity master. Resolve merge conflicts, if there are any."
-git fetch parity master
-git merge parity/master
-echo "OK"
