8001239afc91a9831bd90a9584797a0142f18ca6
ron <yrong1997@gmail.com>
2024-06-12 10:01:55 +0800

    Use a simple EMA instead

--- bridges/snowbridge/pallets/gas-price/src/lib.rs
+++ bridges/snowbridge/pallets/gas-price/src/lib.rs
@@ -7,23 +7,16 @@ pub mod mock;
 #[cfg(test)]
 mod test;
 
-use frame_support::pallet_prelude::{ConstU32, ValueQuery};
+use frame_support::pallet_prelude::ValueQuery;
 use frame_system::WeightInfo;
 pub use pallet::*;
-use snowbridge_core::{BaseFeePerGas, GasPriceProvider, RingBufferMap, RingBufferMapImpl};
+use snowbridge_core::{BaseFeePerGas, GasPriceProvider};
 use sp_core::{Get, U256};
+use sp_runtime::{FixedU128, Saturating};
+
 pub const LOG_TARGET: &str = "gas-price";
 
-pub const BUFFER_SIZE: u32 = 50;
-pub const TWAP_INTERVAL: u32 = 3;
-pub type CumulatedPriceBuffer<T> = RingBufferMapImpl<
-	u32,
-	ConstU32<BUFFER_SIZE>,
-	PriceIndex<T>,
-	PriceStateMapping<T>,
-	PriceState<T>,
-	ValueQuery,
->;
+const BLENDING_FACTOR: FixedU128 = FixedU128::from_rational(20, 100);
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -52,64 +45,42 @@ pub mod pallet {
 	/// Gas price
 	#[pallet::storage]
 	#[pallet::getter(fn gas_price)]
-	pub(super) type InstantGasPrice<T: Config> = StorageValue<_, BaseFeePerGas, ValueQuery>;
-
-	#[pallet::storage]
-	pub type PriceIndex<T: Config> = StorageValue<_, u32, ValueQuery>;
-
-	#[pallet::storage]
-	pub type PriceStateMapping<T: Config> = StorageMap<_, Identity, u32, u64, ValueQuery>;
-
-	#[pallet::storage]
-	pub type PriceState<T: Config> = StorageMap<_, Identity, u64, U256, ValueQuery>;
+	pub(super) type AccumulatedGasPrice<T: Config> = StorageValue<_, BaseFeePerGas, ValueQuery>;
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {}
 
 	impl<T: Config> Get<U256> for Pallet<T> {
 		fn get() -> U256 {
-			let index = PriceIndex::<T>::get();
-			let slot = <PriceStateMapping<T>>::get(index);
-			let value = <CumulatedPriceBuffer<T>>::get(slot);
-
-			let previous_index;
-			if index >= TWAP_INTERVAL {
-				previous_index = index - TWAP_INTERVAL;
-			} else {
-				previous_index = index + BUFFER_SIZE - TWAP_INTERVAL;
-			}
-			let previous_slot = <PriceStateMapping<T>>::get(previous_index);
-			let previous_value = <CumulatedPriceBuffer<T>>::get(previous_slot);
-
-			let price: U256;
-			if previous_slot > 0 && slot > previous_slot {
-				price = value
-					.saturating_sub(previous_value)
-					.checked_div(U256::from(slot - previous_slot))
-					.expect("divisor is non-zero; qed")
-					.into();
-			} else {
-				price = InstantGasPrice::<T>::get().value
-			}
-			price
+			AccumulatedGasPrice::<T>::get().value
 		}
 	}
 
 	impl<T: Config> GasPriceProvider for Pallet<T> {
 		fn update(value: U256, slot: u64) {
-			let previous_index = PriceIndex::<T>::get();
-			let previous_slot = <PriceStateMapping<T>>::get(previous_index);
-			let previous_value = <PriceState<T>>::get(previous_slot);
-			let cumulated_value: U256;
-			if previous_slot > 0 && slot > previous_slot {
-				cumulated_value = previous_value
-					.saturating_add(value.saturating_mul(U256::from(slot - previous_slot)));
+			let cumulated_value: U256 = <AccumulatedGasPrice<T>>::get().value;
+
+			let fixed_value = FixedU128::from_inner(value.low_u128());
+			let cumulated_fixed_value = FixedU128::from_inner(cumulated_value.low_u128());
+
+			let cumulated_fixed_value_updated: FixedU128;
+			if fixed_value > cumulated_fixed_value {
+				cumulated_fixed_value_updated = cumulated_fixed_value.saturating_add(
+					fixed_value
+						.saturating_sub(cumulated_fixed_value)
+						.saturating_mul(BLENDING_FACTOR),
+				);
 			} else {
-				cumulated_value = U256::zero()
+				cumulated_fixed_value_updated = cumulated_fixed_value.saturating_sub(
+					cumulated_fixed_value
+						.saturating_sub(fixed_value)
+						.saturating_mul(BLENDING_FACTOR),
+				);
 			}
 
-			<CumulatedPriceBuffer<T>>::insert(slot, cumulated_value);
-			<InstantGasPrice<T>>::set(BaseFeePerGas { value, slot });
+			let cumulated_value = U256::from(cumulated_fixed_value_updated.into_inner());
+			<AccumulatedGasPrice<T>>::set(BaseFeePerGas { value: cumulated_value, slot });
+
 			Self::deposit_event(Event::Updated { value, cumulated_value, slot });
 		}
 	}
--- bridges/snowbridge/pallets/gas-price/src/test.rs
+++ bridges/snowbridge/pallets/gas-price/src/test.rs
@@ -7,18 +7,21 @@ fn update_value_works() {
 		GasPrice::update(U256::from(1000), 10);
 		GasPrice::update(U256::from(2000), 20);
 		let price = GasPrice::get();
-		assert_eq!(price, 2000.into());
+		assert_eq!(price, 560.into());
 		GasPrice::update(U256::from(3000), 30);
 		let price = GasPrice::get();
-		assert_eq!(price, 3000.into());
+		assert_eq!(price, 1048.into());
 		GasPrice::update(U256::from(4000), 40);
 		let price = GasPrice::get();
-		assert_eq!(price, 3000.into());
+		assert_eq!(price, 1638.into());
 		GasPrice::update(U256::from(4000), 50);
 		let price = GasPrice::get();
-		assert_eq!(price, 3666.into());
-		GasPrice::update(U256::from(4000), 60);
+		assert_eq!(price, 2110.into());
+		GasPrice::update(U256::from(3000), 60);
 		let price = GasPrice::get();
-		assert_eq!(price, 4000.into());
+		assert_eq!(price, 2288.into());
+		GasPrice::update(U256::from(2000), 70);
+		let price = GasPrice::get();
+		assert_eq!(price, 2231.into());
 	});
 }
