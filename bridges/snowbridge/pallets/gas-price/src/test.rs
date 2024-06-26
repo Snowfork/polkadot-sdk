c498edf05a4fa12bac5dccd23b3c49e636c172f1
ron <yrong1997@gmail.com>
2024-06-26 10:14:13 +0800

    More tests

--- bridges/snowbridge/pallets/gas-price/src/test.rs
+++ bridges/snowbridge/pallets/gas-price/src/test.rs
@@ -10,19 +10,27 @@ fn update_value_works() {
 		GasPrice::update(gwei(25), 30);
 		let price = GasPrice::get();
 		assert_eq!(price, gwei(25));
+
+		// Update with price increased, the updated value should be more than the previous but less
+		// than the updating one
 		GasPrice::update(gwei(30), 40);
 		let price = GasPrice::get();
 		assert_eq!(price, gwei(26));
 
 		// Update with price decreased, the updated value should be less than the previous but more
-		// than the new one
+		// than the updating one
 		GasPrice::update(gwei(20), 50);
 		let price = GasPrice::get();
 		assert_eq!(price, 24800000000_u128.into());
 
 		// Update with a large interval, the new value should dominate the EMA
-		GasPrice::update(gwei(30), 50 + 8192);
+		GasPrice::update(gwei(3), 50 + 8192);
 		let price = GasPrice::get();
-		assert_eq!(price, gwei(30).into());
+		assert_eq!(price, gwei(3).into());
+
+		// Update with an outdated price, the new value won't change
+		GasPrice::update(gwei(5), 50 + 8192 - 5);
+		let price = GasPrice::get();
+		assert_eq!(price, gwei(3).into());
 	});
 }
