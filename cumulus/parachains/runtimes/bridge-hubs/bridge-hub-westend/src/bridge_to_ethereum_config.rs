62c0868f439db5e3d155b6f34154f6f1a2b4cc92
ron <yrong1997@gmail.com>
2024-07-21 23:58:54 +0800

    Add Payer to westend

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/bridge_to_ethereum_config.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/bridge_to_ethereum_config.rs
@@ -91,6 +91,8 @@ impl snowbridge_pallet_inbound_queue::Config for Runtime {
 	type WeightInfo = crate::weights::snowbridge_pallet_inbound_queue::WeightInfo<Runtime>;
 	type PricingParameters = EthereumSystem;
 	type AssetTransactor = <xcm_config::XcmConfig as xcm_executor::Config>::AssetTransactor;
+
+	type Payer = EthereumOutboundQueue;
 }
 
 impl snowbridge_pallet_outbound_queue::Config for Runtime {
